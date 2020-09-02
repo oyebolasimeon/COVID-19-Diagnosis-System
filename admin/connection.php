9fb065014f4d31f92c0f5dba5acc56fbf81fab88
Oyebola Simeon Oyekunle <oyebolasimeon@gmail.com>
2020-09-01 20:07:02 -0700

    Delete connection.php

--- admin/connection.php
+++
@@ -1,8 +0,0 @@
- <?php
- $db_connect = new mysqli("localhost", "root", "", "dcigcomn_covid");
-      if ($db_connect->connect_error) {
-        die("Could not connect to the databse: ". $conn->connect_error);
-        }
-?>
-
-
